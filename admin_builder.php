ce34044b3d6f427b7668d6bee42a57d1791f9796
Ciprian Turcu <turcuciprian1@gmail.com>
2016-06-14 22:55:24 +0300

    including the export file if it exists

--- admin_builder.php
+++ admin_builder.php
@@ -1,14 +1,7 @@
 <?php
-/*
-  Plugin Name: Admin Builder
-  Plugin URI: http://admin-builder.com
-  Description: A plugin that generates admin panel pages & posts, meta boxes and fields (with unlimited textbox, textarea, checkbox, custom select (dropdown box), datepicker, timepicker, colorpicker, upload media fields, with configurable options)
-  Version: 1.0.6
-  Author: rootabout
-  Author URI: http://admin-builder.com
-  License: GPLv2 or later
-  Text Domain: aB
- */
-
  require_once('inc/abEnqueue.php');
  require_once('inc/abCore.php');
+	$exportFile = 'abExport.php';
+	if(is_file($exportFile)){
+		require_once($exportFile);
+	}
