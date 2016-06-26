6e0a889da0af0c6c5fa914e4c2b448c6d86c8f4f
Ciprian Turcu <turcuciprian1@gmail.com>
2016-06-27 01:44:26 +0200

    update 1.0.3 ,restoring the old functionality from the library

--- admin_builder.php
+++ admin_builder.php
@@ -1,4 +1,13 @@
 <?php
-global $rootURL;
-require_once 'inc/abEnqueue.php';
-require_once 'inc/abCore.php';
+/*
+  Plugin Name: Admin Builder
+  Plugin URI: http://admin-builder.com
+  Description: A plugin that generates admin panel pages & posts, meta boxes and fields (with unlimited textbox, textarea, checkbox, custom select (dropdown box), datepicker, timepicker, colorpicker, upload media fields, with configurable options)
+  Version: 1.0.2.10
+  Author: rootabout
+  Author URI: http://admin-builder.com
+  License: GPLv2 or later
+  Text Domain: aB
+ */
+ require_once('inc/enqueue.php');
+ require_once('inc/core.php');
