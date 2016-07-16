4826c31e1bedd0687abc5a3c3c7f7728a1b13420
Ciprian Turcu <turcuciprian1@gmail.com>
2016-07-16 16:38:46 +0300

    added init hook to routes

--- inc/abRoutes.php
+++ inc/abRoutes.php
@@ -1,7 +1,21 @@
 <?php
+
 if (!class_exists('abRoutes')) {
     class abRoutes
     {
-
+        private $generalArr;
+      /**
+       * Start up.
+       */
+      public function __construct($generalArr)
+      {
+        global $abGen;
+          $this->generalArr = $generalArr;
+          // $abGen->showArr($generalArr);
+          add_action('init',array($this,'abRoutesInit'));
+      }
+      public function abRoutesInit(){
+        
+      }
     }
 }
